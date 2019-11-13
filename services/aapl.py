bfe40b9dd76a2a0fa95535c41e03eb2ed2585d02
Alain Garcia <alaingarcia@ufl.edu>
2019-11-13 16:19:11 -0500

    Fix requirements?

--- services/aapl.py
+++ services/aapl.py
@@ -3,13 +3,14 @@ import http.client
 import json
 import MySQLdb
 
+ticker="AAPL"
 key = "A5dHAZqYNutmBOjIzppnWIsAwYw4"
 db_host = "35.237.245.229"
 db_user = "root"
 db_password = "admin"
 
 # get latest price when given ticker using tradier's example code
-def getLatestPrice(key, ticker="AAPL"):
+def getLatestPrice(key=key, ticker=ticker):
     # Request: Market Quotes (https://sandbox.tradier.com/v1/markets/quotes?symbols=spy)
     connection = http.client.HTTPSConnection('sandbox.tradier.com', 443, timeout = 30)
     # Headers
@@ -21,7 +22,7 @@ def getLatestPrice(key, ticker="AAPL"):
         content = response.read()
         # Success
         return json.loads(content)["quotes"]["quote"]["last"]
-    except http.client.HTTPException as e:
+    except http.client.HTTPException:
         # Exception
         print('Exception during request')
 
