ad9e50c0e7e98f815040313e9c5fc6728c5ab1c1
Alain Garcia <alaingarcia@ufl.edu>
2019-11-13 16:12:22 -0500

    Successful connection to db
    - Made services folder, to be used for each individual microservice
    - Created aapl.py, a microservice
    - Connected to a database that is using Cloud SQL (Google App Engine)

--- services/aapl.py
+++ services/aapl.py
@@ -1,15 +1,19 @@
-from iexfinance.stocks import Stock
 import pandas as pd
 import http.client
 import json
+import MySQLdb
 
-# get latest price when given ticker
-def getLatestPrice(ticker):
+key = "A5dHAZqYNutmBOjIzppnWIsAwYw4"
+db_host = "35.237.245.229"
+db_user = "root"
+db_password = "admin"
+
+# get latest price when given ticker using tradier's example code
+def getLatestPrice(key, ticker="AAPL"):
     # Request: Market Quotes (https://sandbox.tradier.com/v1/markets/quotes?symbols=spy)
     connection = http.client.HTTPSConnection('sandbox.tradier.com', 443, timeout = 30)
     # Headers
-    headers = {"Accept":"application/json",
-            "Authorization":"Bearer A5dHAZqYNutmBOjIzppnWIsAwYw4"}
+    headers = {"Accept":"application/json", "Authorization":"Bearer "+key}
     # Send synchronously
     connection.request('GET', '/v1/markets/quotes?symbols={}'.format(ticker), None, headers)
     try:
@@ -21,4 +25,4 @@ def getLatestPrice(ticker):
         # Exception
         print('Exception during request')
 
-print(getLatestPrice("AAPL"))
+db = MySQLdb.connect(host=db_host,user=db_user,password=db_password)
