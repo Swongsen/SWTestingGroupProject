ec42fa7a2b4e0003566a097545aba05c17bb72f8
Alain Garcia <alaingarcia@ufl.edu>
2019-11-14 18:07:12 -0500

    Changed to mysql-connector-python

--- services/connect.py
+++ services/connect.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import mysql.connector
 
 # Config values
 db_host = "34.73.169.74"
@@ -7,7 +7,7 @@ db_password = "cis4930"
 
 # Connects to database, set up cursor
 try:
-    db = MySQLdb.connect(host=db_host,user=db_user,password=db_password)
+    db = mysql.connector.connect(host=db_host,user=db_user,password=db_password)
 except:
     print("Could not connect to OBS database, is it up?")
 
