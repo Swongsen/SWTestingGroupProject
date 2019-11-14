9aacebb40d41e80b18484846ff1745f7b2c54466
Alain Garcia <alaingarcia@ufl.edu>
2019-11-14 18:41:52 -0500

    Added logging functionality for stock transactions

--- services/monitoring.py
+++ services/monitoring.py
@@ -10,12 +10,18 @@ cur.execute("USE logs")
 # type should be either: 
 # authentication, transaction, or stock transaction
 def log(table, information):
+    # information format = (userid, username)
     if table == "authentication":
         cur.execute("CREATE TABLE IF NOT EXISTS authentication(userid INTEGER NOT NULL, username TEXT NOT NULL, created_at TEXT NOT NULL)")
         cur.execute("INSERT INTO authentication(userid, username, created_at) VALUES ({},'{}',NOW())".format(information[0],information[1]))
     elif table == "transaction":
         return 1
+    # information format = (ticker, userid, accountid, type, amount, price)
     elif table == "stocktransaction":
+        cur.execute("CREATE DATABASE IF NOT EXISTS {}".format(information[0]))
+        cur.execute("USE {}".format(information[0]))
+        cur.execute("CREATE TABLE IF NOT EXISTS transactions(userid INTEGER NOT NULL, accountid INTEGER NOT NULL, type TEXT NOT NULL, amount INTEGER NOT NULL, price DOUBLE NOT NULL, created_at TEXT NOT NULL)")
+        cur.execute("INSERT INTO transactions(userid, accountid, type, amount, price, created_at) VALUES ({},{},{},{},{},NOW())".format(information[0],information[1],information[2],information[3],information[4],information[5]))
         return 1
     return 1
 
