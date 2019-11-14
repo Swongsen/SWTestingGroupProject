e14f2b1b782a60a1da8ee9d81d9680b95bd8269b
Spencer <swongsen@ufl.edu>
2019-11-14 17:54:38 -0500

    more tests

--- auth/authtests.py
+++ auth/authtests.py
@@ -4,9 +4,13 @@ import unittest
 from unittest.mock import Mock
 from auth import login, createAccount
 
-def test_login():
-    assert 1==1
 
+def test_login():
+    mock = Mock()
+    mock.login()
+    mock.login.assert_called()
 
 def test_createAccount():
-    assert 1==1
+    mock = Mock()
+    mock.createAccount()
+    mock.createAccount.assert_called()
