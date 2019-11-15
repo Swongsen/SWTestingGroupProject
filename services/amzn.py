08f8d1601555a6354158ea720a0620c2eeca54b4
Georgia <gahayes97@ufl.edu>
2019-11-14 22:17:04 -0500

    Update amzn.py

--- services/amzn.py
+++ services/amzn.py
@@ -1,19 +1,27 @@
 import pandas as pd
 import http.client
 import json
+from flask import jsonify
 from services import connect
 
 # Config values
 ticker="amzn"
-key = "A5dHAZqYNutmBOjIzppnWIsAwYw4"
+tradier_key = "A5dHAZqYNutmBOjIzppnWIsAwYw4"
 
 cur = connect.cursor
 # get latest price when given ticker using tradier's example code
-def getLatestPrice(key=key, ticker=ticker.upper()):
+def getLatestPrice(key, tradier_key=tradier_key, ticker=ticker.upper()):
+    key = int(key)
+    cur.execute("USE accounts")
+    cur.execute("SELECT accountid FROM accounts WHERE accountid={}".format(key))
+    result = cur.fetchone()
+    if result == None:
+        return "Invalid API key"
+
     # Request: Market Quotes (https://sandbox.tradier.com/v1/markets/quotes?symbols=spy)
     connection = http.client.HTTPSConnection('sandbox.tradier.com', 443, timeout = 30)
     # Headers
-    headers = {"Accept":"application/json", "Authorization":"Bearer "+key}
+    headers = {"Accept":"application/json", "Authorization":"Bearer "+tradier_key}
     # Send synchronously
     connection.request('GET', '/v1/markets/quotes?symbols={}'.format(ticker), None, headers)
     try:
@@ -25,34 +33,60 @@ def getLatestPrice(key=key, ticker=ticker.upper()):
         # Exception
         print('Exception during request')
 
-def buy(session, amount):
-    # get accountid from accountname and userid
-    accountname = session["token"]
-    userid = session["userid"]
+def buy(amount, key):
+    key = int(key)
     cur.execute("USE accounts")
-    cur.execute("SELECT accountid FROM accounts WHERE accountname = '{}' AND userid = {}".format(accountname, userid))
-    account = cur.fetchone()[0]
+    cur.execute("SELECT accountid FROM accounts WHERE accountid={}".format(key))
+    result = cur.fetchone()
+    if result == None:
+        return "Invalid API key"
 
     # log transaction
     cur.execute("CREATE DATABASE IF NOT EXISTS {}".format(ticker))
     cur.execute("USE {}".format(ticker))
-    price = getLatestPrice()
+    price = getLatestPrice(key)
     cur.execute("CREATE TABLE IF NOT EXISTS transactions(accountid INTEGER NOT NULL, type TEXT NOT NULL, amount INTEGER NOT NULL, price DOUBLE NOT NULL, created_at TEXT NOT NULL)")
-    cur.execute("INSERT INTO transactions(accountid, type, amount, price, created_at) VALUES ({}, 'buy', {}, {}, NOW())".format(account, amount, price))
+    cur.execute("INSERT INTO transactions(accountid, type, amount, price, created_at) VALUES ({}, 'buy', {}, {}, NOW())".format(key, amount, price))
 
     #update accounts database
     cur.execute("USE accounts")
-    cur.execute("SELECT funds, {} FROM accounts WHERE accountid = {}".format(ticker, account))
+    cur.execute("SELECT funds, {} FROM accounts WHERE accountid = {}".format(ticker, key))
     results = cur.fetchone()
-    
+
     #subtract funds from account, add stocks to account
     total_funds = float(results[0])
     total_stocks = int(results[1])
     funds_left = total_funds - (price * float(amount))
     stocks_added = total_stocks + float(amount)
-    print("UPDATE accounts SET funds = {} AND {} = {} WHERE accountid = {}".format(funds_left, ticker, stocks_added, account))
-    cur.execute("UPDATE accounts SET funds = {}, {} = {} WHERE accountid = {}".format(funds_left, ticker, stocks_added, account))
+    print("UPDATE accounts SET funds = {} AND {} = {} WHERE accountid = {}".format(funds_left, ticker, stocks_added, key))
+    cur.execute("UPDATE accounts SET funds = {}, {} = {} WHERE accountid = {}".format(funds_left, ticker, stocks_added, key))
     return "Successfully bought {} stocks of {}".format(amount, ticker)
-        raise Exception("Invalid")
 
-getLatestPrice()
+def sell(amount, key):
+    key = int(key)
+    cur.execute("USE accounts")
+    cur.execute("SELECT accountid FROM accounts WHERE accountid={}".format(key))
+    result = cur.fetchone()
+    if result == None:
+        return "Invalid API key"
+
+    # log transaction
+    cur.execute("CREATE DATABASE IF NOT EXISTS {}".format(ticker))
+    cur.execute("USE {}".format(ticker))
+    price = getLatestPrice(key)
+    cur.execute("CREATE TABLE IF NOT EXISTS transactions(accountid INTEGER NOT NULL, type TEXT NOT NULL, amount INTEGER NOT NULL, price DOUBLE NOT NULL, created_at TEXT NOT NULL)")
+    cur.execute("INSERT INTO transactions(accountid, type, amount, price, created_at) VALUES ({}, 'buy', {}, {}, NOW())".format(key, amount, price))
+
+    #update accounts database
+    cur.execute("USE accounts")
+    cur.execute("SELECT funds, {} FROM accounts WHERE accountid = {}".format(ticker, key))
+    results = cur.fetchone()
+
+    #subtract funds from account, add stocks to account
+    total_funds = float(results[0])
+    total_stocks = int(results[1])
+    funds_left = total_funds + (price * float(amount))
+    stocks_added = total_stocks - float(amount)
+    print("UPDATE accounts SET funds = {} AND {} = {} WHERE accountid = {}".format(funds_left, ticker, stocks_added, key))
+    cur.execute("UPDATE accounts SET funds = {}, {} = {} WHERE accountid = {}".format(funds_left, ticker, stocks_added, key))
+    return "Successfully bought {} stocks of {}".format(amount, ticker)
