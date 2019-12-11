7761d3f80af1ed9e5e615df0147ed574dbbda840
Spencer <swongsen@ufl.edu>
2019-12-11 16:17:57 -0500

    delete tests folder (obsolete)

--- tests/servicestests.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import os
-import unittest
-from unittest.mock import Mock
-from obs.py import addAccount, viewAccountBalance, addFunds, buyShare, sellShare, netWorth
-from monitoring.py import log, viewAuthenticationLogs
-from aapl.py import getLatestPrice
