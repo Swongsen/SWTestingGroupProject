c9279a5b30d6d9e4e1c422f0474a7b39c7344a2e
Alain Garcia <alaingarcia@ufl.edu>
2019-12-11 17:43:48 -0500

    Update connect.py

--- services/connect.py
+++ services/connect.py
@@ -1,16 +1,18 @@
 import mysql.connector
 
 # Config values for staging
+"""
 db_host = "34.73.169.74"
 db_user = "root"
 db_password = "cis4930"
-
 """
+
+
 # Config values for production
 db_host = "34.74.182.197"
 db_user = "root"
 db_password = "cis4930"
-"""
+
 
 # Connects to database, set up cursor
 try:
